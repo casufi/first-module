669ec9565b560bbe66ee1b74a8c7bf53ac9d9e3f
Volodymyr Kotulskyi <vkotulskyi@lohika.com>
2020-02-07 17:36:03 +0200

    Improving structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import First from "./components/First"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from "react-router-dom";
 
-function App() {
+import Home from "./components/Home"
+import Dashboard from "./components/Dashboard"
+
+const FirstRoutes = (props) => {
+  let { path, url } = useRouteMatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Test component</h1>
-      </header>
-      <First/>
+    <div>
+      <ul>
+        <li>
+          <Link to={`${url}/`}>Start</Link>
+        </li>
+        <li>
+          <Link to={`${url}/dashboard`}>Dashboard</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path={path}>
+          <Home/>
+        </Route>
+        <Route path={`${path}/dashboard`}>
+          <Dashboard/>
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
+
+const App = (props) => {
+
+  return (
+    <div className="card">
+      <Router>
+        <FirstRoutes/>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
